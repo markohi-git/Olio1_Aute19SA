770ee8a55e87a45d8981a7b9d1491ec50e047c07
k5000455 <k5000455@C-A2203-00>
2021-01-05 15:34:27 +0200

    Add project files.

---
+++ Olio1_Aute19SA/Henkilo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio1_Aute19SA
+{
+    class Henkilo
+    {
+        //jäsenmuuttujat
+        private string Etunimi;
+        private string Sukunimi;
+        private string Kotipaikka;
+
+        //metodi saa parametrina merkkijonon, mutta ei palauta mitään (void)
+        public void setEtunimi(string e)
+        {
+            Etunimi = e;
+        }
+        public void setSukunimi(string s)
+        {
+            Sukunimi = s;
+        }
+        public void setKotipaikka(string k)
+        {
+            Kotipaikka = k;
+        }
+
+        //metodi palauttaa merkkijonon
+        public string nimi()
+        {
+            return Etunimi + " " + Sukunimi;
+        }
+        public string kotipaikka()
+        {
+            return Kotipaikka;
+        }
+
+    }
+}
---
+++ Olio1_Aute19SA/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio1_Aute19SA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            //Paikka johon tallennetaan "henkilöitä"
+            ArrayList a = new ArrayList();
+            //luetaan rivejä niin kauan että rivinä on vain kirjain q
+            while ((input = Console.ReadLine()) != "q")
+            {
+                //Luodaan uusi henkilö olio
+                Henkilo h = new Henkilo();
+                //slip pilkkoo merkkijonon taulukoksi
+                h.setEtunimi(input.Split(' ')[0]);
+                h.setSukunimi(input.Split(' ')[1]);
+                a.Add(h);
+            }
+            Console.WriteLine("Henkilöitä on " + a.Count);
+            //käydään läpi arraylist
+            for (int i = 0; i < a.Count; i++) 
+            {
+                //otetaan arraylististä olio joka on i indeksissä
+                Henkilo h = (Henkilo)a[i];
+                Console.WriteLine("Henkilö "+i+" "+h.nimi());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
